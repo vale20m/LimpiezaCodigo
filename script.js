91047f738e1de63d4e598e623d6c176f60ef9725
Vale20m <valentinveintemilla@gmail.com>
2023-09-13 21:09:00 -0300

    Codigo comentado

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var boton = document.getElementById("btn");
-var box = document.getElementById("caja");
+const button = document.getElementById("btn");
+const colorContainer = document.getElementById("caja");
 
-function CambiarColor() {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+function changeColor() {
+    let hexadecimalDigits = "0123456789ABCDEF"; // Cadena de caracters que contiene todos los caracteres hexadecimales
+    let color = "#"; // "#" se utiliza para inicializar un codigo de color hexadecimal
+    for (let i = 0; i < 6; i++) {
+        color += hexadecimalDigits[Math.floor(Math.random() * 16)]; // Forma un codigo de color de 16 digitos hexadecimales
     }
-    caja.style.backgroundColor = color;
+    caja.style.backgroundColor = color; // Establece la variable "color" como color de fondo de "colorContainer"
 }
 
-boton.addEventListener("click", CambiarColor);
+button.addEventListener("click", changeColor); // Al presionar el boton, se ejecuta la funcion "changeColor"
